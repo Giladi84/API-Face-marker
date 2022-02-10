e5172d919af5660bb935c136b2ac4ff85ec1cc24
Adi <at.pro.quality@gmail.com>
2022-02-10 11:42:30 +0200

    port change

--- server.js
+++ server.js
@@ -27,11 +27,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
+app.get('/', (req,res) => console.log('Server be working!!!'))
 app.post('/signin', (req,res) => signin.handleSignIn(req,res,knex,bcrypt));
 app.post('/register', (req,res) => register.handleRegister(req,res,knex,bcrypt))
 app.put('/image', (req,res) => image.handleImage(req,res,knex))
 app.post('/imageUrl', (req,res) => image.handleClarifaiCall(req,res,fetch))
 
 
-app.listen(3001 , () => console.log('working!!!!!'))
+app.listen(process.env.PORT || 3000 , () => console.log('working!!!!!'))
